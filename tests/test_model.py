70d37bd5fc41bf28d61bc2580c255eabec35c52e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-05-05 20:25:27 +0000

    one more test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@88081 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- tests/test_model.py
+++ tests/test_model.py
@@ -8,9 +8,13 @@ __docformat__ = "restructuredtext en"
 # pylint: disable=W0212,R0904
 
 from hamcrest import is_
+from hamcrest import none
 from hamcrest import all_of
+from hamcrest import is_not
 from hamcrest import has_entry
+from hamcrest import has_length
 from hamcrest import assert_that
+from hamcrest import has_property
 
 from nti.testing.matchers import verifiably_provides
 
@@ -19,6 +23,7 @@ import unittest
 from nti.invitations.interfaces import IInvitation
 
 from nti.invitations.model import Invitation
+from nti.invitations.model import InvitationsContainer
 
 from nti.invitations.tests import SharedConfiguringTestLayer
 
@@ -56,3 +61,16 @@ class TestModel(unittest.TestCase):
 		assert_that(invitation.is_email(), is_(False))
 		invitation.receiver = 'ichigo@bleach.org'
 		assert_that(invitation.is_email(), is_(True))
+		
+	def test_container(self):
+		invitation = Invitation(code='bleach',
+								receiver='ichigo',
+								inviter='aizen',
+								accepted=True)
+		container = InvitationsContainer()
+		container.add(invitation)
+		assert_that(container, has_length(is_(1)))
+		assert_that(invitation, has_property('code', is_not(none())))
+		
+		container.remove(invitation)
+		assert_that(container, has_length(is_(0)))
