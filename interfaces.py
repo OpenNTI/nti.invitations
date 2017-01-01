bf93eb075515a7c98aef755061e8033e15194cee
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-01 17:46:48 +0000

    pep 8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@103389 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- interfaces.py
+++ interfaces.py
@@ -31,8 +31,9 @@ from zope.lifecycleevent.interfaces import IObjectModifiedEvent
 
 from zope.schema import ValidationError
 
-from nti.coremetadata.interfaces import ICreated
-from nti.coremetadata.interfaces import ILastModified
+from nti.base.interfaces import ICreated
+from nti.base.interfaces import ILastModified
+
 from nti.coremetadata.interfaces import SYSTEM_USER_NAME
 
 from nti.invitations import MessageFactory as _
@@ -45,163 +46,179 @@ from nti.schema.field import Object
 from nti.schema.field import ValidText
 from nti.schema.field import ValidTextLine
 
+
 class IInvitation(IContained,
-				  ICreated,
-				  ILastModified):
-	"""
-	An invitation from one user of the system (or the system itself)
-	for another user to be able to do something.
+                  ICreated,
+                  ILastModified):
+    """
+    An invitation from one user of the system (or the system itself)
+    for another user to be able to do something.
 
-	Invitations are initially created and registered with an
-	:class:`IInvitations` utility. At some time in the future, someone
-	who was invited may accept the invitation. The process of
-	accepting the invitation is considered to run at the credential
-	level of the creator of the invitation (thus allowing accepting
-	the invitation to do things like join a group of the creator).
+    Invitations are initially created and registered with an
+    :class:`IInvitations` utility. At some time in the future, someone
+    who was invited may accept the invitation. The process of
+    accepting the invitation is considered to run at the credential
+    level of the creator of the invitation (thus allowing accepting
+    the invitation to do things like join a group of the creator).
 
-	Invitations may expire after a period of time and/or be good for only
-	a certain number of uses. They may have a predicate that determines they are
-	applicable only to certain users (for example, a list of invited users).
-	"""
+    Invitations may expire after a period of time and/or be good for only
+    a certain number of uses. They may have a predicate that determines they are
+    applicable only to certain users (for example, a list of invited users).
+    """
 
-	code = ValidTextLine(title="A unique code that identifies this invitation.",
-						 required=False)
+    code = ValidTextLine(title="A unique code that identifies this invitation.",
+                         required=False)
 
-	receiver = ValidTextLine(title="Invitation receiver (username or email).",
-						  	 required=True)
+    receiver = ValidTextLine(title="Invitation receiver (username or email).",
+                             required=True)
 
-	sender = ValidTextLine(title="Invitation sender.",
-						   required=True, default=SYSTEM_USER_NAME)
+    sender = ValidTextLine(title="Invitation sender.",
+                           required=True, default=SYSTEM_USER_NAME)
 
-	message = ValidText(title="Invitation message.", required=False)
-	
-	accepted = Bool(title="Accepted flag.", default=False, required=True)
+    message = ValidText(title="Invitation message.", required=False)
 
-	expiryTime = Number(title="The expiry timestamp.", required=True, default=0)
+    accepted = Bool(title="Accepted flag.", default=False, required=True)
 
-	sent = Number(title="The sent timestamp.", required=False)
-	sent.setTaggedValue('_ext_excluded_out', True)
-	
-	def is_email():
-		"""
-		Returns true if the receiver is an email address
-		"""
+    expiryTime = Number(
+        title="The expiry timestamp.", required=True, default=0)
 
-	def is_expired():
-		"""
-		Returns true if this invitation has expired
-		"""
+    sent = Number(title="The sent timestamp.", required=False)
+    sent.setTaggedValue('_ext_excluded_out', True)
 
-	def is_accepted():
-		"""
-		Returns true if the invitation has been accepted
-		"""
-		
+    def is_email():
+        """
+        Returns true if the receiver is an email address
+        """
+
+    def is_expired():
+        """
+        Returns true if this invitation has expired
+        """
+
+    def is_accepted():
+        """
+        Returns true if the invitation has been accepted
+        """
+
+
 class IInvitationsContainer(IContained,
-							IContainer,
-				   			IAnnotatable):
-	"""
-	A central registry of invitations. Intended to be used as a utility registered
-	for the site.
-	"""
-	contains(str('.IInvitation'))
+                            IContainer,
+                            IAnnotatable):
+    """
+    A central registry of invitations. Intended to be used as a utility registered
+    for the site.
+    """
+    contains(str('.IInvitation'))
 
-	def add(invitation):
-		"""
-		Registers the given invitation with this object. This object is responsible for
-		assigning the invitation code and taking ownership of the invitation.
-		"""
-	registerInvitation = add
+    def add(invitation):
+        """
+        Registers the given invitation with this object. This object is responsible for
+        assigning the invitation code and taking ownership of the invitation.
+        """
+    registerInvitation = add
 
-	def remove(invitation):
-		"""
-		Remove the given invitation with this object.
-		"""
-	removeInvitation = remove
+    def remove(invitation):
+        """
+        Remove the given invitation with this object.
+        """
+    removeInvitation = remove
 
-	def getInvitationByCode(code):
-		"""
-		Returns an invitation having the given code, or None if there is no
-		such invitation.
-		"""
-IInvitations = IInvitationsContainer # BWC
-		
+    def get_invitation_by_code(code):
+        """
+        Returns an invitation having the given code, or None if there is no
+        such invitation.
+        """
+    getInvitationByCode = get_invitation_by_code
+
+IInvitations = IInvitationsContainer  # BWC
+
+
 class IInvitationEvent(IObjectEvent):
-	"""
-	An event specifically about an invitation.
-	"""
-	object = Object(IInvitation, title="The invitation.")
+    """
+    An event specifically about an invitation.
+    """
+    object = Object(IInvitation, title="The invitation.")
+
 
 class IInvitationSentEvent(IInvitationEvent):
-	"""
-	An invitation has been sent.
-	"""
-	user = interface.Attribute("The user to whom the invitation is sent.")
+    """
+    An invitation has been sent.
+    """
+    user = interface.Attribute("The user to whom the invitation is sent.")
+
 
 @interface.implementer(IInvitationSentEvent)
 class InvitationSentEvent(ObjectEvent):
 
-	receiver = alias('user')
+    receiver = alias('user')
 
-	def __init__(self, obj, user):
-		super(InvitationSentEvent, self).__init__(obj)
-		self.user = user
+    def __init__(self, obj, user):
+        super(InvitationSentEvent, self).__init__(obj)
+        self.user = user
+
 
 class IInvitationAcceptedEvent(IObjectModifiedEvent, IInvitationEvent):
-	"""
-	An invitation has been accepted.
-	"""
-	user = interface.Attribute("The user that accepted the invitation.")
+    """
+    An invitation has been accepted.
+    """
+    user = interface.Attribute("The user that accepted the invitation.")
+
 
 @interface.implementer(IInvitationAcceptedEvent)
 class InvitationAcceptedEvent(ObjectModifiedEvent):
 
-	def __init__(self, obj, user):
-		super(InvitationAcceptedEvent, self).__init__(obj)
-		self.user = user
+    def __init__(self, obj, user):
+        super(InvitationAcceptedEvent, self).__init__(obj)
+        self.user = user
+
 
 class InvitationValidationError(ValidationError):
-	"""
-	A problem relating to the validity of an attempted action on
-	an invitation.
-	"""
+    """
+    A problem relating to the validity of an attempted action on
+    an invitation.
+    """
 
-	def __init__(self, invitation=None):
-		super(InvitationValidationError, self).__init__()
-		self.invitation = invitation
+    def __init__(self, invitation=None):
+        super(InvitationValidationError, self).__init__()
+        self.invitation = invitation
+
 
 class InvitationCodeError(InvitationValidationError):
-	__doc__ = _('The invitation is not valid.')
-	i18n_message = __doc__
+    __doc__ = _('The invitation is not valid.')
+    i18n_message = __doc__
+
 
 class InvitationExpiredError(InvitationValidationError):
-	__doc__ = _('The invitation has expired.')
-	i18n_message = __doc__
+    __doc__ = _('The invitation has expired.')
+    i18n_message = __doc__
+
 
 class InvitationAlreadyAcceptedError(InvitationValidationError):
-	__doc__ = _('Invitation already accepted.')
-	i18n_message = __doc__
+    __doc__ = _('Invitation already accepted.')
+    i18n_message = __doc__
+
 
 class InvitationActorError(InvitationValidationError):
-	__doc__ = _('The invitation does not have actor.')
-	i18n_message = __doc__
+    __doc__ = _('The invitation does not have actor.')
+    i18n_message = __doc__
+
 
 class IInvitationActor(interface.Interface):
-	"""
-	An interface for a utility to act on an invitation
-	"""
+    """
+    An interface for a utility to act on an invitation
+    """
 
-	def accept(user, invitation):
-		"""
-		Perform whatever action is required for the ``user`` to accept the invitation, including
- 		validating that the user is actually allowed to accept the invitation. Typically
- 		this means that this method has side effects.
+    def accept(user, invitation):
+        """
+        Perform whatever action is required for the ``user`` to accept the invitation, including
+        validating that the user is actually allowed to accept the invitation. Typically
+        this means that this method has side effects.
 
-		:param user User being invited
-		:param invitation invitation object
-		:return None/False if invitation was not accepted
+        :param user User being invited
+        :param invitation invitation object
+        :return None/False if invitation was not accepted
 
-		Once the invitation has been accepted, this should notify an :class:`IInvitationAcceptedEvent`.
+        Once the invitation has been accepted, this should notify an :class:`IInvitationAcceptedEvent`.
 
- 		:raises InvitationExpiredError: If the invitation has expired.
-		"""
+        :raises InvitationExpiredError: If the invitation has expired.
+        """
--- model.py
+++ model.py
@@ -46,92 +46,96 @@ from nti.schema.field import SchemaConfigured
 
 from nti.schema.fieldproperty import createDirectFieldProperties
 
+
 @WithRepr
 @EqHash('code')
 @interface.implementer(IInvitation, IAttributeAnnotatable, IContentTypeAware)
 class Invitation(PersistentCreatedModDateTrackingObject,
-				 SchemaConfigured):
+                 SchemaConfigured):
 
-	createDirectFieldProperties(IInvitation)
+    createDirectFieldProperties(IInvitation)
 
-	__parent__ = None
-	__name__ = alias('code')
+    __parent__ = None
+    __name__ = alias('code')
 
-	username = alias('receiver')
-	inviter = creator = alias('sender')
-	expirationTime = alias('expiryTime')
-		
-	parameters = {} # IContentTypeAware
+    username = alias('receiver')
+    inviter = creator = alias('sender')
+    expirationTime = alias('expiryTime')
 
-	mimeType = mime_type = u"application/vnd.nextthought.invitation"
+    parameters = {}  # IContentTypeAware
 
-	def __init__(self, *args, **kwargs):
-		SchemaConfigured.__init__(self, *args, **kwargs)
-		PersistentCreatedModDateTrackingObject.__init__(self)
+    mimeType = mime_type = u"application/vnd.nextthought.invitation"
 
-	@readproperty
-	def sender(self):
-		return SYSTEM_USER_NAME
+    def __init__(self, *args, **kwargs):
+        SchemaConfigured.__init__(self, *args, **kwargs)
+        PersistentCreatedModDateTrackingObject.__init__(self)
 
-	def is_email(self):
-		return bool(self.receiver and isValidMailAddress(self.receiver))
-	isEmail = is_email
+    @readproperty
+    def sender(self):
+        return SYSTEM_USER_NAME
 
-	def is_expired(self, now=None):
-		expirationTime = self.expiryTime
-		now = time.time() if not now else now
-		return bool(expirationTime and expirationTime <= now)
-	isExpired = is_expired
-	
-	def is_accepted(self):
-		return self.accepted
-	isAccepted = is_accepted
+    def is_email(self):
+        return bool(self.receiver and isValidMailAddress(self.receiver))
+    isEmail = is_email
 
-	def __lt__(self, other):
-		try:
-			return (self.code, self.createdTime) < (self.code, self.createdTime)
-		except AttributeError:  # pragma: no cover
-			return NotImplemented
+    def is_expired(self, now=None):
+        expirationTime = self.expiryTime
+        now = time.time() if not now else now
+        return bool(expirationTime and expirationTime <= now)
+    isExpired = is_expired
 
-	def __gt__(self, other):
-		try:
-			return (self.code, self.createdTime) > (self.code, self.createdTime)
-		except AttributeError:  # pragma: no cover
-			return NotImplemented
+    def is_accepted(self):
+        return self.accepted
+    isAccepted = is_accepted
+
+    def __lt__(self, other):
+        try:
+            return (self.code, self.createdTime) < (self.code, self.createdTime)
+        except AttributeError:  # pragma: no cover
+            return NotImplemented
+
+    def __gt__(self, other):
+        try:
+            return (self.code, self.createdTime) > (self.code, self.createdTime)
+        except AttributeError:  # pragma: no cover
+            return NotImplemented
+
 
 @interface.implementer(IInvitationsContainer, IAttributeAnnotatable)
 class InvitationsContainer(CaseInsensitiveLastModifiedBTreeContainer,
-						   Contained):
-	
-	def add(self, invitation):
-		code = invitation.code
-		if not code:
-			code = get_random_invitation_code()
-			while code in self:
-				code = get_random_invitation_code()
-			invitation.code = code
-		self[code] = invitation
-	registerInvitation = append = add
-	
-	def remove(self, invitation):
-		code = getattr(invitation, 'code', invitation)
-		if code in self:
-			del self[code]
-			return True
-		return False
-	removeInvitation = remove
-	
-	def getInvitationByCode(self, code):
-		return self.get(code)
+                           Contained):
+
+    def add(self, invitation):
+        code = invitation.code
+        if not code:
+            code = get_random_invitation_code()
+            while code in self:
+                code = get_random_invitation_code()
+            invitation.code = code
+        self[code] = invitation
+    registerInvitation = append = add
+
+    def remove(self, invitation):
+        code = getattr(invitation, 'code', invitation)
+        if code in self:
+            del self[code]
+            return True
+        return False
+    removeInvitation = remove
+
+    def get_invitation_by_code(self, code):
+        return self.get(code)
+    getInvitationByCode = get_invitation_by_code
+
 
 def install_invitations_container(site_manager_container, intids=None):
-	lsm = site_manager_container.getSiteManager()
-	intids = lsm.getUtility(IIntIds) if intids is None else intids
-	registry = lsm.queryUtility(IInvitationsContainer)
-	if registry is None:
-		registry = InvitationsContainer()
-		registry.__parent__ = site_manager_container
-		registry.__name__ = '++etc++invitations-container'
-		intids.register(registry)
-		lsm.registerUtility(registry, provided=IInvitationsContainer)
-	return registry
+    lsm = site_manager_container.getSiteManager()
+    intids = lsm.getUtility(IIntIds) if intids is None else intids
+    registry = lsm.queryUtility(IInvitationsContainer)
+    if registry is None:
+        registry = InvitationsContainer()
+        registry.__parent__ = site_manager_container
+        registry.__name__ = '++etc++invitations-container'
+        intids.register(registry)
+        lsm.registerUtility(registry, provided=IInvitationsContainer)
+    return registry
