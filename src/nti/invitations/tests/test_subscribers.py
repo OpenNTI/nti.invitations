dc289185ba9c2234ec5e4855af904879b61cdb88
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-17 12:26:52 -0500

    100% coverage

---
+++ src/nti/invitations/tests/test_subscribers.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# pylint: disable=protected-access,too-many-public-methods
+
+from hamcrest import is_
+from hamcrest import none
+from hamcrest import is_not
+from hamcrest import assert_that
+from hamcrest import has_property
+
+from zope.event import notify
+
+from nti.invitations.interfaces import InvitationSentEvent
+
+from nti.invitations.model import UserInvitation
+
+from nti.invitations.tests import InvitationLayerTest
+
+
+class TestSubscribers(InvitationLayerTest):
+
+    def test_time(self):
+        shikai = UserInvitation(code='shikai',
+                                receiver='zangetzu',
+                                sender='ichigo',
+                                accepted=True)
+        assert_that(shikai, has_property('sent', is_(none())))
+        notify(InvitationSentEvent(shikai, 'zangetzu'))
+        assert_that(shikai, has_property('sent', is_not(none())))
