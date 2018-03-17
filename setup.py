e93d8c0d3e678f7c3e50629a29181f74e264b51f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-17 11:27:44 -0500

    add supporting files

---
+++ setup.py
@@ -0,0 +1,70 @@
+import codecs
+from setuptools import setup, find_packages
+
+entry_points = {
+    'console_scripts': [
+    ],
+}
+
+TESTS_REQUIRE = [
+    'nti.testing',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.invitations',
+    version=_read('version.txt').strip(),
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI invitations",
+    long_description=(
+        _read('README.rst') 
+        + '\n\n' 
+        + _read("CHANGES.rst")
+    ),
+    license='Apache',
+    keywords='invitations',
+    classifiers=[
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.invitations",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'six',
+        'zope.component',
+        'zope.deferredimport',
+        'zope.deprecation',
+        'zope.interface',
+        'zope.schema'
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    entry_points=entry_points,
+)
