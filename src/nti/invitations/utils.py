059db76865940a1f8dcc026c32684e98f027d045
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-07 18:10:43 -0500

    Add sites param to expired/pending/sent invitations functions.

--- src/nti/invitations/utils.py
+++ src/nti/invitations/utils.py
@@ -8,12 +8,13 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-import six
 import time
 import uuid
 from datetime import datetime
 
 from BTrees.LFBTree import LFSet
+
+import six
 
 from zope import component
 
@@ -42,6 +43,14 @@ MAX_TS = time.mktime(datetime.max.timetuple())
 logger = __import__('logging').getLogger(__name__)
 
 
+def safe_iterable(value, sep=','):
+    if isinstance(value, six.string_types):
+        value = value.split(sep)
+    value = set(value or ())
+    value.discard(None)
+    return value or ()
+
+
 def get_random_invitation_code():
     s = str(uuid.uuid4()).split('-')[-1].upper()
     result = s[0:4] + '-' + s[4:8] + '-' + s[8:]
@@ -58,25 +67,20 @@ def get_invitation_actor(invitation, user=None):
 def get_invitations_ids(sites=None, receivers=None, senders=None, catalog=None):
     query = {}
     catalog = get_invitations_catalog() if catalog is None else catalog
-
     # add sites
-    if isinstance(sites, six.string_types):
-        sites = sites.split()
+    sites = safe_iterable(sites)
     if sites:
         query[IX_SITE] = {'any_of': sites}
-
     # add senders and receivers
     for name, values in ((IX_RECEIVER, receivers), (IX_SENDER, senders)):
         if values:
-            if isinstance(values, six.string_types):
-                values = values.split()
+            values = safe_iterable(values)
             query[name] = {'any_of': values}
-
     # run query if available
     if query:
         doc_ids = catalog.apply(query) or ()
     else:
-        doc_ids = list(catalog[IX_SITE].documents_to_values.keys())
+        doc_ids = tuple(catalog[IX_SITE].documents_to_values.keys())
     return doc_ids
 
 
@@ -95,36 +99,34 @@ def get_invitations(sites=None, receivers=None, senders=None, catalog=None):
     return result
 
 
-def get_pending_invitation_ids(receivers=None, now=None, catalog=None):
-    if isinstance(receivers, six.string_types):
-        receivers = set(receivers.split(","))
-    elif receivers:
-        receivers = set(receivers)
+def get_pending_invitation_ids(receivers=None, sites=None, now=None, catalog=None):
     catalog = get_invitations_catalog() if catalog is None else catalog
     query = {
         IX_ACCEPTED: {'any_of': (False,)},
         IX_EXPIRYTIME: {'any_of': (0,)},
     }
+    # check sites
+    sites = safe_iterable(sites)
+    if sites:
+        query[IX_SITE] = {'any_of': sites}
+    # check receivers
+    receivers = safe_iterable(receivers)
     if receivers:
-        receivers.discard(None)
         query[IX_RECEIVER] = {'any_of': receivers}
-
     # pending no expiry
     no_expire_ids = catalog.apply(query) or LFSet()
-
     # pending with expiration
     now = time.time() if not now else now
     query[IX_EXPIRYTIME] = {'between': (now, MAX_TS)}
     in_between_ids = catalog.apply(query) or LFSet()
-
     # return union
     return catalog.family.IF.multiunion([no_expire_ids, in_between_ids])
 
 
-def get_pending_invitations(receivers=None, now=None, catalog=None):
+def get_pending_invitations(receivers=None, sites=None, now=None, catalog=None):
     result = []
     intids = component.getUtility(IIntIds)
-    doc_ids = get_pending_invitation_ids(receivers, now, catalog)
+    doc_ids = get_pending_invitation_ids(receivers, sites, now, catalog)
     for uid in doc_ids or ():
         obj = intids.queryObject(uid)
         if is_actionable(obj):
@@ -132,9 +134,9 @@ def get_pending_invitations(receivers=None, now=None, catalog=None):
     return result
 
 
-def has_pending_invitations(receivers=None, now=None, catalog=None):
+def has_pending_invitations(receivers=None, sites=None, now=None, catalog=None):
     intids = component.getUtility(IIntIds)
-    doc_ids = get_pending_invitation_ids(receivers, now, catalog)
+    doc_ids = get_pending_invitation_ids(receivers, sites, now, catalog)
     for uid in doc_ids or ():
         obj = intids.queryObject(uid)
         if is_actionable(obj):
@@ -142,11 +144,7 @@ def has_pending_invitations(receivers=None, now=None, catalog=None):
     return False
 
 
-def get_expired_invitation_ids(receivers=None, now=None, catalog=None):
-    if isinstance(receivers, six.string_types):
-        receivers = set(receivers.split(","))
-    elif receivers:
-        receivers = set(receivers)
+def get_expired_invitation_ids(receivers=None, sites=None, now=None, catalog=None):
     # 60 min value w/ minute resolution
     now = time.time() - 60 if not now else now
     catalog = get_invitations_catalog() if catalog is None else catalog
@@ -155,17 +153,20 @@ def get_expired_invitation_ids(receivers=None, now=None, catalog=None):
         # 60 min value w/ minute resolution
         IX_EXPIRYTIME: {'between': (60, now)},
     }
+    sites = safe_iterable(sites)
+    if sites:
+        query[IX_SITE] = {'any_of': sites}
+    receivers = safe_iterable(receivers)
     if receivers:
-        receivers.discard(None)
         query[IX_RECEIVER] = {'any_of': receivers}
     expired_ids = catalog.apply(query) or LFSet()
     return expired_ids
 
 
-def get_expired_invitations(receivers=None, now=None, catalog=None):
+def get_expired_invitations(receivers=None, sites=None, now=None, catalog=None):
     result = []
     intids = component.getUtility(IIntIds)
-    doc_ids = get_expired_invitation_ids(receivers, now, catalog)
+    doc_ids = get_expired_invitation_ids(receivers, sites, now, catalog)
     for uid in doc_ids or ():
         obj = intids.queryObject(uid)
         if is_actionable(obj):
@@ -173,35 +174,34 @@ def get_expired_invitations(receivers=None, now=None, catalog=None):
     return result
 
 
-def delete_expired_invitations(receivers=None, now=None, catalog=None):
+def delete_expired_invitations(receivers=None, sites=None, now=None, catalog=None):
     result = []
     container = component.getUtility(IInvitationsContainer)
-    invitations = get_expired_invitations(receivers, now, catalog)
+    invitations = get_expired_invitations(receivers, sites, now, catalog)
     for invitation in invitations:
         if container.remove(invitation):
             result.append(invitation)
     return result
 
 
-def get_sent_invitation_ids(senders, accepted=False, catalog=None):
-    if isinstance(senders, six.string_types):
-        senders = set(senders.split(","))
-    elif senders:
-        senders = set(senders)
-    senders.discard(None)
+def get_sent_invitation_ids(senders, sites=None, accepted=False, catalog=None):
+    senders = safe_iterable(senders)
     catalog = get_invitations_catalog() if catalog is None else catalog
     query = {
         IX_SENDER: {'any_of': senders},
         IX_ACCEPTED: {'any_of': (accepted,)},
     }
+    sites = safe_iterable(sites)
+    if sites:
+        query[IX_SITE] = {'any_of': sites}
     expired_ids = catalog.apply(query) or LFSet()
     return expired_ids
 
 
-def get_sent_invitations(senders, accepted=False, catalog=None):
+def get_sent_invitations(senders, sites=None, accepted=False, catalog=None):
     result = []
     intids = component.getUtility(IIntIds)
-    doc_ids = get_sent_invitation_ids(senders, accepted, catalog)
+    doc_ids = get_sent_invitation_ids(senders, sites, accepted, catalog)
     for uid in doc_ids or ():
         obj = intids.queryObject(uid)
         if is_actionable(obj):
